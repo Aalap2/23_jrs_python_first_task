f1a21a7cef8221bd906bd447986e48efa0a83bf6
Aalap2 <137003310+Aalap2@users.noreply.github.com>
2023-06-19 12:50:40 +0530

    Add files via upload

---
+++ qn1.py
@@ -0,0 +1,9 @@
+num =int( input("Enter a number:"))
+count = 0
+while(num>0):
+    count +=1
+    num=num//10
+    print("Digits in  the number are:",str(count))
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ qn3.py
@@ -0,0 +1,10 @@
+#Computers ={
+   # "Laptop1":{"brand":"DELL","OS":"Windows"},
+    #"Laptop2":{"brand":"HP","OS":"Linux"}
+    #"Laptop3":{"brand":"Apple","OS":"MAC-OS"}}
+brand = ["DEll","Hp","Apple"]
+for i in range(0,3,1):
+    print(brand[i])
+OS = ["Windows","Linux","MAC-OS"]
+for j in range(0,3,1):
+    print(OS[j])
